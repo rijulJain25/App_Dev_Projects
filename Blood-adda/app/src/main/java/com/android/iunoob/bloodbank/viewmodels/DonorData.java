c8803bfc1a9d2679c8104a1caeb5d76cff8f62db
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-06-10 01:25:58 +0530

    Add files via upload

---
+++ Blood-adda/app/src/main/java/com/android/iunoob/bloodbank/viewmodels/DonorData.java
@@ -0,0 +1,69 @@
+package com.android.iunoob.bloodbank.viewmodels;
+
+public class DonorData {
+
+    private int TotalDonate;
+    private String LastDonate, Name, Contact, UID, Address;
+
+
+    public DonorData() {
+
+    }
+
+    public DonorData(int totalDonate, String lastDonate, String Name, String Contact, String Address, String UID) {
+        this.TotalDonate = totalDonate;
+        this.LastDonate = lastDonate;
+        this.Name = Name;
+        this.Contact = Contact;
+        this.UID = UID;
+        this.Address = Address;
+    }
+
+    public int getTotalDonate() {
+        return TotalDonate;
+    }
+
+    public void setTotalDonate(int totalDonate) {
+        this.TotalDonate = totalDonate;
+    }
+
+    public String getLastDonate() {
+        return LastDonate;
+    }
+
+    public void setLastDonate(String lastDonate) {
+        this.LastDonate = lastDonate;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setDonorName(String donorName) {
+        this.Name = Name;
+    }
+
+    public String getContact() {
+        return Contact;
+    }
+
+    public void setContact(String donorContact) {
+        this.Contact = Contact;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+}
